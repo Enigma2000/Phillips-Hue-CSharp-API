b89fc1ac82511cc2bd02142b1d145cd55c25e28b
Enigma <44625841+EnigmaKM@users.noreply.github.com>
2020-01-17 18:40:55 -0600

    Initial Creation

---
+++ Phillips Hue CSharp API/Phillips Hue CSharp API/JSONizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phillips_Hue_CSharp_API
+{
+    public class JSONizer
+    {
+        public static string JSONize()
+        {
+            return "";
+        }
+    }
+}
---
+++ Phillips Hue CSharp API/Phillips Hue CSharp API/LightState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phillips_Hue_CSharp_API
+{
+    class LightState
+    {
+    }
+}
